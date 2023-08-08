96c3c861ecc02bb1fc85d3dc5a7bcdb94fc207e4
Larisa Kindalova <l.kindalova@yandex.ru>
2023-08-08 16:26:07 +1000

    fix:port & DB

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 ## Ссылки на проект
 
-IP-адрес 158.160.63.119
+IP 158.160.63.119
 
 Frontend https://kind.nomoreparties.co
 
--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const cors = require('cors');
 
-const { PORT = 3000, MONGO_DB } = process.env;
+const { PORT = 3000, MONGO_DB = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const rateLimit = require('express-rate-limit'); // Для защиты от DoS-атак //https://www.npmjs.com/package/express-rate-limit
 const helmet = require('helmet'); // https://expressjs.com/ru/advanced/best-practice-security.html
